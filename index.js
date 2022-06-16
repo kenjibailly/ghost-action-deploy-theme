34712513a9e8ce71e4cf8a5cb478aac188011d61
Kenji Bailly <kenjibailly@gmail.com>
2022-06-16 14:49:26 +0200

    update max length

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ const GhostAdminApi = require('@tryghost/admin-api');
                     params,
                     data,
                     headers,
-                    maxContentLength: 9999999999999999999999999999999999999999999999999999999999999999999999999999999999999,
-                    maxBodyLength: 9999999999999999999999999999999999999999999999999999999999999999999999999999999999999,
+                    maxContentLength: 500000000,
+                    maxBodyLength: 500000000,
                     paramsSerializer(parameters) {
                         return Object.keys(parameters).reduce((parts, key) => {
                             const val = encodeURIComponent([].concat(parameters[key]).join(','));
